b9e805475cdf615f6d321aa4f5f6e8db91b3d28a
Miquel Grau <miquelg3@gmail.com>
2022-01-21 10:17:51 +0100

    Tutorial-react

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Tabla from './Tabla';
+import Form from './Form';
+
+class App extends Component {
+  state = {
+    personas: [
+      // {
+      //   nombre: 'Clark',
+      //   apellido: 'Kent',
+      // },
+      // {
+      //   nombre: 'Bruce',
+      //   apellido: 'Wayne',
+      // },
+      // {
+      //   nombre: 'Peter',
+      //   apellido: 'Parker',
+      // },
+      // {
+      //   nombre: 'Selina',
+      //   apellido: 'Kyle',
+      // },
+    ],
+  }
+  eliminarPersona = (indice) => {
+    const { personas } = this.state
+  
+    this.setState({
+      personas: personas.filter((personas, i) => {
+        return i !== indice;
+      }),
+    });
+  }
+  enviarFormulario = (persona) => {
+    this.setState({ personas: [...this.state.personas, persona] });
+  }
+  render() {
+    const { personas } = this.state;
+  
+    return (
+      <div className="container">
+        <h1>Tutorial de React de Neoguias!</h1>
+        <Tabla datosPersonas={personas} eliminarPersona={this.eliminarPersona} />
+        <Form enviarFormulario={this.enviarFormulario} />
+      </div>
+    )
+  }
+}
+export default App;
